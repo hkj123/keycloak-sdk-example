666985cae684554311ddc8a823c3b103947f07cc
hukaijia <190916516@qq.com>
2019-07-11 17:33:08 +0800

    11

---
+++ keycloak-jetty-example/src/main/java/org/keycloak/example/UserServlet.java
@@ -0,0 +1,31 @@
+package org.keycloak.example;
+
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.example.utils.CommonKeys;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        Keycloak keycloak = this.passwordToken();
+        System.out.println(keycloak);
+        response.getWriter().println("<h1>Hello from HelloServlet"+keycloak+"</h1>");
+    }
+
+    public Keycloak passwordToken() {
+        Keycloak keycloak = Keycloak.getInstance(CommonKeys.KEYCLOAK_AUTH_SERVER_URL, CommonKeys.KEYCLOAK_REALM, CommonKeys.USER_NAME,
+                CommonKeys.PASSWORD, CommonKeys.CLIENT_ID, CommonKeys.CLIENT_SECRET);
+        return keycloak;
+    }
+}
---
+++ keycloak-jetty-example/src/main/java/org/keycloak/example/utils/CommonKeys.java
@@ -0,0 +1,18 @@
+package org.keycloak.example.utils;
+
+public class CommonKeys {
+    //启动端口
+    public static final Integer SERVER_PORT = 8090;
+    //realm
+    public static final String KEYCLOAK_REALM = "spring-boot-quickstart";
+    //auth-server-url
+    public static final String KEYCLOAK_AUTH_SERVER_URL = "http://localhost:8180/auth";
+    //USER_NAME
+    public static final String USER_NAME= "alice";
+    //password
+    public static final String PASSWORD= "password";
+    //client_Id
+    public static final String CLIENT_ID= "app-authz-springboot";
+    //client_Secret
+    public static final String CLIENT_SECRET= "e6bc5c7c-ec63-43c5-a4d7-593f0de0cdcb";
+}
