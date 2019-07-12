1fb97354a6997b5b2b629c89929d94fca658f3f8
hukaijia <190916516@qq.com>
2019-07-12 11:21:07 +0800

    jetty示例

--- keycloak-jetty-example/src/main/java/org/keycloak/example/UserServlet.java
+++ keycloak-jetty-example/src/main/java/org/keycloak/example/UserServlet.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
 
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
